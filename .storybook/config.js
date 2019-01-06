6941933da802ccf7fd667f204611c3d402a4752c
Tim Reynolds <tim@timothyreynolds.co.uk>
2019-01-06 20:09:26 +0000

    Add percy

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,15 @@
-import { configure } from "@storybook/react";
+import { configure, getStorybook, setAddon } from "@storybook/react";
+import createPercyAddon from "@percy-io/percy-storybook";
+
+const { percyAddon, serializeStories } = createPercyAddon();
 
 function loadStories() {
   require("../stories/components/Button");
   require("../stories/components/Heading");
 }
 
+setAddon(percyAddon);
+
 configure(loadStories, module);
+
+serializeStories(getStorybook);
